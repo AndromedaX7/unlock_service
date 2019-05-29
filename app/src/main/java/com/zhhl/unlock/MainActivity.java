db2e777c710b908ba1629bf430c65f5347ad5874
andromedaX7 <andromeda7@126.com>
2019-05-29 09:15:02 +0800

    transparentStatusBar

--- app/src/main/java/com/zhhl/unlock/MainActivity.java
+++ app/src/main/java/com/zhhl/unlock/MainActivity.java
@@ -1,13 +1,33 @@
 package com.zhhl.unlock;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+        transparentStatus();
+    }
+
+
+    void transparentStatus() {
+        if (Build.VERSION.SDK_INT >= 16) {
+            final int option = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().getDecorView().setSystemUiVisibility(option);
+            if (Build.VERSION.SDK_INT>21){
+                getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                getWindow().setStatusBarColor(Color.TRANSPARENT);
+            }
+
+        }
+
+    }
 }
